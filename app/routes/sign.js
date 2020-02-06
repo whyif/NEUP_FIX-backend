6f293f7cc4e0bc4a85fac042e1d07e5bd7f133a2
leevensu <leevensu@gmail.com>
2020-02-07 00:29:26 +0800

    add comment and fix info(router)

--- app/routes/sign.js
+++ app/routes/sign.js
@@ -5,6 +5,7 @@ const router = express.Router();
 
 const NEUfix= require('../db/client').db('NEU_fix');
 const user =NEUfix.collection('user')
+/*此处则是对数据库的client的导入，并且collection */
 
 
 //-----------登录-------------------------------------
@@ -18,6 +19,12 @@ router.get('/signin',function(req,res){
       }
   
 })
+
+/*
+上面的get：
+    先验证会话，对于已经登陆的用户无法重复登陆，
+    没有登陆的，则渲染登陆界面 signin 
+*/
 
 router.post('/signin',function(req,res){    
   user.findOne({username:req.body.username}).then((result) =>{   
@@ -39,6 +46,7 @@ router.post('/signin',function(req,res){
 router.get('/signup',function(req,res){
     if(req.session.username){
         res.send(req.session.username+'you have sign in')
+        
       }else{
           console.log('please log')
         res.render('signup')
@@ -46,13 +54,23 @@ router.get('/signup',function(req,res){
       }
   
 })
+/*
+    上面的：get
+    先验证会话，对于已经登陆的用户无法注册，
+    没有登陆的，则渲染登陆界面 signup
+*/
+
+
+
 router.post('/signup',function(req,res){
     let sinupData=req.body
     var flag=true
+
     for(let i in sinupData){
         if(!sinupData[i]){
             flag=false
         }
+        //for循环检测提交的数据是否有空白情况，有则不合格，flag为false，不允许插入数据
     }
 user.findOne({username:req.body.username}).then((result)=>{
   if(result===null&&flag){
@@ -69,7 +87,8 @@ user.findOne({username:req.body.username}).then((result)=>{
           }else{
               //res.status(200).end()
               req.session.username=req.body.username
-              res.redirect('http://localhost:8080/home')
+              res.redirect('/home')
+              //注册成功自动建立会话，并重定向到/home
           }
       })
   }else{
@@ -78,9 +97,19 @@ user.findOne({username:req.body.username}).then((result)=>{
   }
 })
 })
-
-router.put('/home/signup',function(req,res){    //修改个人信息,有待修改
-
+//注销账号
+router.get('/signout',function(req,res){
+    if(req.session.username){
+        delete req.session.username
+        res.status(200).end();
+    }else{
+        res.redirect('/signin')
+    }
+    //如果得到收到/signou 的get请求，则删除会话的session达到注销目的
 })
+
+
+
+//修改用户信息我改到了info页面
 
 module.exports = router;
--- app/routes/user/info.js
+++ app/routes/user/info.js
@@ -1,18 +1,73 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/info',function(req,res){
+const NEUfix= require('../../db/client').db('NEU_fix');
+const user =NEUfix.collection('user')
+/*更新数据要提交到库，故还是要 连接数据库 */
+
+router.get('/info',function(req,res,next){
   
-  res.send(req.session.username+'your page')
+  res.render('userInfo')
+  next()
+})
+/*get请求渲染页面，post时更新数据到数据库 */
+
+router.post('/info',function(req,res){
+      if(req.session.username){
+        let upData=req.body
+        let flag=true
+
+        for(let i in upData){//判断数据是否有空
+          if(!upData[i]){
+              flag=false
+          }
+
+          user.findOne({username:upData.username,password:upData.oldpassword}).then(()=>{
+            if(flag){
+              user.updateOne({username:upData.username},{$set:upData}).then((result)=>{
+                if (result.result.nModified === 1) {
+                  res.status(200).end("update successful.")
+              } else if (result.result.n === 1) {
+                  res.status(200).end("no update performed");
+              } else {
+                  res.status(404).end('no such user')
+              }
+              })//此部分为Ctrl C， CTRL V来的，我还在学习中，但是功能已经实现，对更新数据的判断
+              //同样的，用户存在？，密码原确？检查数据空？，这个操作会产生一个oldpassword的数据
+          }
+          
+        })
+      }
+    }
 })
 
+/*
+由于实验目的需要暂时写成这样，到时候会恢复，
+同理对会话的验证，以及对于修改后的数据的检查，
+最后，暂时不清楚put方法是怎么实现的故暂时用post进行处理
+对数据的更改，
+*/
+
+
+
+
+
+
+/*
+实验战略注释
 router.get('/info/:userid', function(req, res) {
   console.log('userInfo page')
   res.render('userInfo')
 })
+*/
 
+//修改个人信息
+/*鉴于测试目的暂时将这些代码注释
 router.put('/info/:userid',function(req,res){
+
   console.log('userinfo change')
 })
+
+*/
 
 module.exports = router;
