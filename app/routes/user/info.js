af7b80576515888a40af8aeea50d615cdbb708af
Maxli <limingxian525@163.com>
2020-02-20 15:31:21 +0800

    修复bugapp/routes/user/apply.js

--- app/routes/user/info.js
+++ app/routes/user/info.js
@@ -1,34 +1,52 @@
+//调试成功，并且使用了objectid作为userID使用
+//唯一的问题还是session
 const express = require('express');
 const router = express.Router();
-const NEUfix=require('../../db/client').db('NEU_fix')
+const NEUfix=require('../../db/client').db('myproject')
 const user=NEUfix.collection('user')
 
-/*更新数据要提交到库，故还是要 连接数据库 */
-
-router.get('/info',function(req,res){
-  user.findOne({userid:req.session.userid}).then((reslut)=>{
-    if(reslut==null){res.sendStatus(405).end()}
-    else{res.sendStatus(200).json(reslut).end()}
-  })
-  console.log(result)
-  //res.send(req.session.username+'your page')
-  //res.render('userInfo')
+router.get('/',function(req,res){
+  res.render('../views/userinfo')
 })
-/*get请求渲染页面，post时更新数据到数据库 */
+router.get('/:userid',function(req,res){
+  user.findOne({userid:req.params.userid}).then((reslut)=>{
+    if(reslut==null){
+      res.send('user not found')//res.sendStatus(405).end()
+    }
+    else{
+      console.log(reslut)//res.sendStatus(200).json(reslut).end()
+    }
+  })
+})
 
-router.post('/info',function(req,res){
-      if(req.session.username){
-        let upData=req.body
-        let flag=true
-
-        for(let i in upData){//判断数据是否有空
-          if(!upData[i]){
-              flag=false
-          }
-
-          user.findOne({username:upData.username,password:upData.oldpassword}).then(()=>{
+router.post('/',function(req,res){
+    let upData=req.body
+    let flag=true
+    for(let i in upData){//判断数据是否有空
+      if(!upData[i]){
+        flag=false
+      }
+    }
+    if(flag){
+      user.findOne({username:req.body.username}).then((result)=>{        //！！！！！这里本应该使用session查找，但是session暂时不会用就先用req.body！！！！！
+        console.log(result)
+        if(result.password==req.body.oldpassword){
+          user.updateOne({ObjectId:result.ObjectId},{$set:req.body},function(err,result){
+            if(err){
+              res.send(err)//.sendStatus(400).end()
+            }
+            else{
+              res.send('information has been modified')  //res.sendStatus(200).end()
+            }
+          })
+        }
+        else{res.send('wrong pwd')}
+      })
+    }      
+    
+    /*user.findOne({username:upData.username,password:upData.oldpassword}).then(()=>{
             if(flag){
-              user.updateOne({username:upData.username},{$set:upData}).then((result)=>{
+              user.updateOne({username:upData.username,password:upData.oldpassword},{$set:upData}).then((result)=>{
                 if (result.result.nModified === 1) {
                   res.status(200).end("update successful.")
               } else if (result.result.n === 1) {
@@ -40,48 +58,8 @@ router.post('/info',function(req,res){
               //同样的，用户存在？，密码原确？检查数据空？，这个操作会产生一个oldpassword的数据
           }
           
-        })
-      }
-    }
-})
-
-/*
-由于实验目的需要暂时写成这样，到时候会恢复，
-同理对会话的验证，以及对于修改后的数据的检查，
-最后，暂时不清楚put方法是怎么实现的故暂时用post进行处理
-对数据的更改，
-*/
-
-/*router.get('/info/:userid', function(req, res) {
-  console.log('userInfo page')
-  res.render('userInfo')
-})*/            //个人觉得只需要用session就够了，毕竟要先登录再看个人信息
-
-
-/*
-实验战略注释
-router.get('/info/:userid', function(req, res) {
-  console.log('userInfo page')
-  res.render('userInfo')
-})
-*/
-
-
-//修改个人信息
-/*鉴于测试目的暂时将这些代码注释
-router.put('/info/:userid',function(req,res){
-<<<<<<< HEAD
-  user.updateOne({userid:req.query.userid},{$set:req.body},function(err,reslut){
-    if(reslut==null){res.sendStatus(400).end()}
-    else{
-      if(err){res.sendStatus(405).end()}
-      else{res.sendStatus(200).end()}
-    }
-  })
-
-  console.log('userinfo change')
-})
-
-*/
+        })*/
+      
+    })
 
 module.exports = router;
